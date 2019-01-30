c9c1af99e84cc9137c1f61707ffe744506b1a798
safi5739 <safi5739@colorado.edu>
2019-01-30 13:13:06 -0700

    Adding the shell scripts.

---
+++ lab2_scripts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Authors : Sam Fitzsimmons
+# Date: 1/30/2019
+
+#Problem 1 Code:
+
+# First, ask for an expression and file
+echo "Enter a regular expression: "
+read regExp
+echo "Enter a file name: "
+read fileName
+
+# Search for given expression
+
+grep '$regExp' $fileName
+
+# Grep for phone number with the default 10 digit format 
+grep -E "^[0-9]{3}.[0-9]{3}.[0-9]{4}$" $fileName | wc -l
+
+# Grep for an email with the given 'character @ character' format
+grep -E "[a-zA-Z0-9]@[a-zA-Z0-9]" $fileName | wc -l
+
+# Grep for a string starting with 303- and output to new txt file
+grep -E "^303-" $fileName > phone_results.txt
+
+#Grep for a string with format 'character @geocities.com' and output to new txt file
+#Confirmed with the sample file that 
+grep -E "[a-zA-Z0-9]@geocities.com" $fileName > email_results.txt
+
+#Ask for follow up command line regular expression
+echo "Enter the Command Line RegEx to search for: "
+read clregex
+
+#Search for expression and output to txt file
+grep -Hn "$clregex" $fileName > command_results.txt
